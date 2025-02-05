c2b14a1f08c95b2b7e7e5a276a299c0ce9440313
Backup158 <beekoop158+gh@gmail.com>
2025-02-05 01:38:53 -0600

    Added support to remove the skull divider icon

--- LoadScreenDecorationRemover/scripts/mods/LoadScreenDecorationRemover/LoadScreenDecorationRemover.lua
+++ LoadScreenDecorationRemover/scripts/mods/LoadScreenDecorationRemover/LoadScreenDecorationRemover.lua
@@ -2,13 +2,51 @@ local mod = get_mod("LoadScreenDecorationRemover")
 mod.version = "1.0"
 mod:info("LoadScreenDecorationRemover v" .. mod.version .. " loaded uwu nya :3")
 
--- #################################################################################
--- Text Removal
--- scripts/ui/views/loading_view/loading_view.lua
---  Replaces the [SPACE] Next prompt with an empty string
---  Don't need to localize something that I'll just replace later, so the input key and text and commented out
--- #################################################################################
+
+local definition_path = "scripts/ui/views/loading_view/loading_view_definitions"
+local UIWidget = require("scripts/managers/ui/ui_widget")
 mod:hook_require("scripts/ui/views/loading_view/loading_view", function(LoadingView)
+    -- #################################################################################
+    -- Divider Removal
+    --  The little ---V--- thing between the next prompt and hints
+    --  Definitions are stored in:       scripts/ui/views/loading_view/loading_view_definitions.lua
+    --      Definitions are called by init, which I hooked
+    --      Replacing the texture with a blank path makes it default to a rectangle that gets colored in
+    --      Making that color have 0 opacity makes it invisible
+    -- #################################################################################
+    mod:hook_safe(LoadingView, "init", function(self, settings, context)
+        self._entry_duration = nil
+	    self._text_cycle_duration = nil
+	    self._update_hint_text = nil
+
+	    local background, background_package = self:select_background()
+	    local definitions = require(definition_path)
+
+	    definitions.widget_definitions.title_divider_bottom = UIWidget.create_definition({
+            {
+                pass_type = "texture",
+                --value = "content/ui/materials/dividers/skull_rendered_center_02",
+                value = "",
+                style = {
+                    -- color = Color.white(255, true),
+                    color = Color.white(0, true),
+                },
+            },
+        }, "title_divider_bottom"),
+        
+
+	    LoadingView.super.init(self, definitions, settings, context, background_package)
+
+	    self._can_exit = context and context.can_exit
+	    self._pass_draw = false
+	    self._no_cursor = true
+    end)
+
+    -- #################################################################################
+    -- Prompt Removal
+    --  Replaces the [SPACE] Next prompt with an empty string
+    --  Don't need to localize something that I'll just replace later, so the input key and text and commented out
+    -- #################################################################################
     mod:hook_safe(LoadingView, "_update_input_display", function(self)
         --local text = "loc_next"
         local widgets_by_name = self._widgets_by_name
@@ -23,14 +61,4 @@ mod:hook_require("scripts/ui/views/loading_view/loading_view", function(LoadingV
         text_widget.content.text = ""
     end)
     
-    --[[
-    -- Current error: attempted to index local 'self' (a function value)
-    mod:hook_safe(LoadingView, "_set_overlay_opacity", function(self, opacity)
-        local widget = self._widgets_by_name.overlay
-
-        widget.alpha_multiplier = 1.0
-    end)
-    ]]
 end)
-
-
