ac75f1814847696633abb33218f5085ffba9f039
RinAnarchy <eubyakuyarinne@gmail.com>
2025-04-12 01:26:11 +0800

    Update 04/12/25 1:26

--- LoadScreenDecorationRemover/scripts/mods/LoadScreenDecorationRemover/LoadScreenDecorationRemover_localization.lua
+++ LoadScreenDecorationRemover/scripts/mods/LoadScreenDecorationRemover/LoadScreenDecorationRemover_localization.lua
@@ -1,32 +1,42 @@
 return {
     mod_name = {
         en = "Load Screen Decoration Remover",
+        ["zh-cn"] = "加载界面修饰移除",
     },
     mod_description = {
         en = "Removes non-background decorations from loading screens. Changes do NOT take effect until game restart/reload.",
+             ["zh-cn"] ="移除加载界面的非背景修饰。 修改将在游戏重启/重载后应用。",
     },
     toggle_hint = {
         en = "Hide Hint",
+        ["zh-cn"] = "隐藏提示文字",
     },
     toggle_hint_description = {
         en = "An open mind is like a fortress with its gates unbarred and unguarded.",
+        ["zh-cn"] = "",
     },
     toggle_divider = {
         en = "Hide Divider",
+        ["zh-cn"] = "隐藏分割线",
     },
     toggle_divider_description = {
         en = "The skull line thing between hints and the prompt.",
+        ["zh-cn"] = "提示文字与按键提示间的骷髅分割线。",
     },
     toggle_prompt = {
         en = "Hide Prompt",
+        ["zh-cn"] = "隐藏按键提示",
     },
     toggle_prompt_description = {
         en = "Text prompt to show the next hint.",
+        ["zh-cn"] = "显示下一项提示文字的按键提示。",
     },
     toggle_skull = {
         en = "Hide Spinning Skull",
+        ["zh-cn"] = "隐藏加载图标",
     },
     toggle_skull_description = {
         en = "Spinning skull logo on the loading screen (including the one before missions). I recommend you leave this on so you know the game hasn't crashed.",
+        ["zh-cn"] = "隐藏加载界面旋转的骷髅图标（包含任务加载界面的骷髅图标图标）。建议保留该图标，这能帮助你确认游戏是否无响应。",
     },
 }
