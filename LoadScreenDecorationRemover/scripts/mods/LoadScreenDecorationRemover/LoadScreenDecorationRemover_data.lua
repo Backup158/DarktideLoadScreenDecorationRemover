0a0a2cb632a83010f9e71f1cb6527d15969a9982
LackupL58 <beekoop158+gh@gmail.com>
2025-09-26 00:10:49 -0600

    style: camel to snake

--- LoadScreenDecorationRemover/scripts/mods/LoadScreenDecorationRemover/LoadScreenDecorationRemover_data.lua
+++ LoadScreenDecorationRemover/scripts/mods/LoadScreenDecorationRemover/LoadScreenDecorationRemover_data.lua
@@ -1,6 +1,6 @@
 local mod = get_mod("LoadScreenDecorationRemover")
 
-local filledWidgets = {}
+local filled_widgets = {}
 --local widgetNames = {"toggle_hint", "toggle_divider", "toggle_prompt"}
 --local widgetNamesDefaultFalse = {"toggle_skull"}
 --local widgetNames = {}
@@ -15,7 +15,7 @@ local function add_widget_to_group(name, group_location, truth)
     }
 end
 local function add_group_widget(name)
-    filledWidgets[#filledWidgets + 1] = {
+    filled_widgets[#filled_widgets + 1] = {
         setting_id = name,
         type = "group",
         sub_widgets = P{},
@@ -24,18 +24,18 @@ end
 
 -- Adds a widget for each one in the list of names
 add_group_widget("toggleable_during_game")
-add_widget_to_group("toggle_hint", filledWidgets.toggleable_during_game, true)
-add_widget_to_group("toggle_prompt", filledWidgets.toggleable_during_game, true)
+add_widget_to_group("toggle_hint", filled_widgets.toggleable_during_game, true)
+add_widget_to_group("toggle_prompt", filled_widgets.toggleable_during_game, true)
 
 add_group_widget("requires_restart")
-add_widget_to_group("toggle_divider", filledWidgets.requires_restart, true)
-add_widget_to_group("toggle_skull", filledWidgets.requires_restart, false)
+add_widget_to_group("toggle_divider", filled_widgets.requires_restart, true)
+add_widget_to_group("toggle_skull", filled_widgets.requires_restart, false)
 
 return {
     name = mod:localize("mod_name"),
     description = mod:localize("mod_description"),
     is_togglable = true,
     options = {
-        widgets = filledWidgets,
+        widgets = filled_widgets,
     },
 }
