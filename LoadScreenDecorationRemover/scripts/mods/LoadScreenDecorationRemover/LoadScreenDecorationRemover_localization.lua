ffdf743a22f1b11d7d99f7b06e1a05a4a689836f
LackupL58 <beekoop158+gh@gmail.com>
2025-09-26 00:19:17 -0600

    local: names for groups

--- LoadScreenDecorationRemover/scripts/mods/LoadScreenDecorationRemover/LoadScreenDecorationRemover_localization.lua
+++ LoadScreenDecorationRemover/scripts/mods/LoadScreenDecorationRemover/LoadScreenDecorationRemover_localization.lua
@@ -4,8 +4,14 @@ return {
         ["zh-cn"] = "加载界面修饰移除",
     },
     mod_description = {
-        en = "Removes non-background decorations from loading screens. Changes do NOT take effect until game restart/reload.",
+        en = "Removes non-background decorations from loading screens.",
         ["zh-cn"] ="移除加载界面的非背景修饰。 修改将在游戏重启/重载后应用。",
+    },
+    toggleable_during_game = {
+        en = "Toggleable Without Restart",
+    },
+    toggleable_during_game_description = {
+        en = "Toggling these widgets while the game is open will have effects on the next loading scren",
     },
     toggle_hint = {
         en = "Hide Hint",
@@ -15,14 +21,6 @@ return {
         en = "An open mind is like a fortress with its gates unbarred and unguarded.",
         ["zh-cn"] = "智而无信，神锋倒持。",
     },
-    toggle_divider = {
-        en = "Hide Divider",
-        ["zh-cn"] = "隐藏分割线",
-    },
-    toggle_divider_description = {
-        en = "The skull line thing between hints and the prompt.",
-        ["zh-cn"] = "提示文字与按键提示间的骷髅分割线。",
-    },
     toggle_prompt = {
         en = "Hide Prompt",
         ["zh-cn"] = "隐藏按键提示",
@@ -30,6 +28,20 @@ return {
     toggle_prompt_description = {
         en = "Text prompt to show the next hint.",
         ["zh-cn"] = "显示下一项提示文字的按键提示。",
+    },
+    requires_restart = {
+        en = "Requires Restart",
+    },
+    requires_restart_description = {
+        en = " Changes do NOT take effect until game restart/reload.",
+    },
+    toggle_divider = {
+        en = "Hide Divider",
+        ["zh-cn"] = "隐藏分割线",
+    },
+    toggle_divider_description = {
+        en = "The skull line thing between hints and the prompt.",
+        ["zh-cn"] = "提示文字与按键提示间的骷髅分割线。",
     },
     toggle_skull = {
         en = "Hide Spinning Skull",
