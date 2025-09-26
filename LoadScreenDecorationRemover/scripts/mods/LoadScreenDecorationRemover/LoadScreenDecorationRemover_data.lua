b0ac567ac04d55a1f43dd490bf03049f9d956d6c
LackupL58 <beekoop158+gh@gmail.com>
2025-09-26 00:10:26 -0600

    style: put into groups

--- LoadScreenDecorationRemover/scripts/mods/LoadScreenDecorationRemover/LoadScreenDecorationRemover_data.lua
+++ LoadScreenDecorationRemover/scripts/mods/LoadScreenDecorationRemover/LoadScreenDecorationRemover_data.lua
@@ -1,27 +1,35 @@
 local mod = get_mod("LoadScreenDecorationRemover")
 
 local filledWidgets = {}
-local widgetNames = {"toggle_hint", "toggle_divider", "toggle_prompt"}
-local widgetNamesDefaultFalse = {"toggle_skull"}
+--local widgetNames = {"toggle_hint", "toggle_divider", "toggle_prompt"}
+--local widgetNamesDefaultFalse = {"toggle_skull"}
 --local widgetNames = {}
 
 -- Appends a toggleable option for a new widget
-local function addWidget(name, truth)
+local function add_widget_to_group(name, group_location, truth)
     -- Write at (table size) + 1, ie inserting at the tail
-    filledWidgets[#filledWidgets + 1] = {
+    group_location[#group_location + 1] = {
         setting_id = name,
         type = "checkbox",
         default_value = truth,
     }
 end
+local function add_group_widget(name)
+    filledWidgets[#filledWidgets + 1] = {
+        setting_id = name,
+        type = "group",
+        sub_widgets = P{},
+    }
+end
 
 -- Adds a widget for each one in the list of names
-for _, name in pairs(widgetNames) do
-    addWidget(name, true)
-end
-for _, name in pairs(widgetNamesDefaultFalse) do
-    addWidgetDefaultFalse(name, false)
-end
+add_group_widget("toggleable_during_game")
+add_widget_to_group("toggle_hint", filledWidgets.toggleable_during_game, true)
+add_widget_to_group("toggle_prompt", filledWidgets.toggleable_during_game, true)
+
+add_group_widget("requires_restart")
+add_widget_to_group("toggle_divider", filledWidgets.requires_restart, true)
+add_widget_to_group("toggle_skull", filledWidgets.requires_restart, false)
 
 return {
     name = mod:localize("mod_name"),
