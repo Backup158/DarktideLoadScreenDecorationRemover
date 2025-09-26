9633b5511ff53fa13b264873ad6ab32d48211183
LackupL58 <beekoop158+gh@gmail.com>
2025-09-25 23:57:55 -0600

    refactor: when hooking all, hook by default then disale ifnot

--- LoadScreenDecorationRemover/scripts/mods/LoadScreenDecorationRemover/LoadScreenDecorationRemover.lua
+++ LoadScreenDecorationRemover/scripts/mods/LoadScreenDecorationRemover/LoadScreenDecorationRemover.lua
@@ -20,13 +20,14 @@ local function hook_the_boys()
     -- Hint Removal
     -- Hints are the loading screen quotes.
     -- #################################################################################
-    if mod:get("toggle_hint") then
-        mod:hook_safe(LoadingView, "_set_hint_text_opacity", function(self, opacity)
-            local widget = self._widgets_by_name.hint_text
+    mod:hook_safe(LoadingView, "_set_hint_text_opacity", function(self, opacity)
+        local widget = self._widgets_by_name.hint_text
 
-            --widget.alpha_multiplier = opacity
-            widget.alpha_multiplier = 0
-        end)
+        --widget.alpha_multiplier = opacity
+        widget.alpha_multiplier = 0
+    end)
+    if not mod:get("toggle_hint") then
+        mod:hook_disable(LoadingView, "_set_hint_text_opacity")
     end
 
     -- #################################################################################
@@ -37,35 +38,36 @@ local function hook_the_boys()
     --      Replacing the texture with a blank path makes it default to a rectangle that gets colored in
     --      Making that color have 0 alpha makes it invisible
     -- #################################################################################
-    if mod:get("toggle_divider") then
-        -- Hook happens before the main body and may not always happen
-        -- vs safe hook, to go back remove the func
-        mod:hook(LoadingView, "init", function(func, self, settings, context)
-            --self._entry_duration = nil
-            --self._text_cycle_duration = nil
-            --self._update_hint_text = nil
-    
-            local background, background_package = self:select_background()
-            local definitions = require(definition_path)
-            
-            definitions.widget_definitions.title_divider_bottom = UIWidget.create_definition({
-                {
-                    pass_type = "texture",
-                    --value = "content/ui/materials/dividers/skull_rendered_center_02",
-                    value = "",
-                    style = {
-                        -- color = Color.white(255, true),
-                        color = Color.white(0, true),
-                    },
-                },
-            }, "title_divider_bottom")
-            
-            LoadingView.super.init(self, definitions, settings, context, background_package)
+    -- Hook happens before the main body and may not always happen
+    -- vs safe hook, to go back remove the func
+    mod:hook(LoadingView, "init", function(func, self, settings, context)
+        --self._entry_duration = nil
+        --self._text_cycle_duration = nil
+        --self._update_hint_text = nil
 
-            --self._can_exit = context and context.can_exit
-            --self._pass_draw = false
-            --self._no_cursor = true
-        end)
+        local background, background_package = self:select_background()
+        local definitions = require(definition_path)
+        
+        definitions.widget_definitions.title_divider_bottom = UIWidget.create_definition({
+            {
+                pass_type = "texture",
+                --value = "content/ui/materials/dividers/skull_rendered_center_02",
+                value = "",
+                style = {
+                    -- color = Color.white(255, true),
+                    color = Color.white(0, true),
+                },
+            },
+        }, "title_divider_bottom")
+        
+        LoadingView.super.init(self, definitions, settings, context, background_package)
+
+        --self._can_exit = context and context.can_exit
+        --self._pass_draw = false
+        --self._no_cursor = true
+    end)
+    if not mod:get("toggle_divider") then
+        mod:hook_disable(LoadingView, "init")
     end
     
     -- #################################################################################
@@ -73,20 +75,21 @@ local function hook_the_boys()
     --  Replaces the [SPACE] Next prompt with an empty string
     --  Hook safe 
     -- #################################################################################
-    if mod:get("toggle_prompt") then
-        mod:hook_safe(LoadingView, "_update_input_display", function(self)
-            --local text = "loc_next"
-            local widgets_by_name = self._widgets_by_name
-            local text_widget = widgets_by_name.hint_input_description
-            --local localized_text = self:_localize(text)
-            local service_type = "View"
-            local alias_name = "next_hint"
-            local color_tint_text = true
-            --local input_key = InputUtils.input_text_for_current_input_device(service_type, alias_name, color_tint_text)
+    mod:hook_safe(LoadingView, "_update_input_display", function(self)
+        --local text = "loc_next"
+        local widgets_by_name = self._widgets_by_name
+        local text_widget = widgets_by_name.hint_input_description
+        --local localized_text = self:_localize(text)
+        local service_type = "View"
+        local alias_name = "next_hint"
+        local color_tint_text = true
+        --local input_key = InputUtils.input_text_for_current_input_device(service_type, alias_name, color_tint_text)
 
-            -- text_widget.content.text = input_key .. " " .. localized_text    -- Original text
-            text_widget.content.text = ""
-        end)
+        -- text_widget.content.text = input_key .. " " .. localized_text    -- Original text
+        text_widget.content.text = ""
+    end)
+    if not mod:get("toggle_prompt") then
+        mod:hook_disable(LoadingView, "_update_input_display")
     end
 
     -- #################################################################################
@@ -120,9 +123,9 @@ end
 local function find_which_hook_to_affect(setting_id)
     -- so this table will be made every time you change settings, but i think that's fine vs having it always exist as the mod runs
     local setting_and_hook_pairs = {
-        toggle_hint = "LoadingView._set_hint_text_opacity",
-        toggle_divider = "LoadingView.init",
-        toggle_prompt = "LoadingView._update_input_display",
+        toggle_hint = "_set_hint_text_opacity",
+        toggle_divider = "init",
+        toggle_prompt = "_update_input_display",
         -- hook origin can't be rehooked, so forget this for the skull
     }
     return setting_and_hook_pairs[setting_id]
@@ -143,8 +146,8 @@ mod.on_setting_changed = function(setting_id)
     --mod:echo(hook_to_affect.." affected")
     -- if setting was enabled and is now disabled (which means show the thing again)
     if not setting_changed_to then
-        mod:hook_disable(hook_to_affect)
+        mod:hook_disable(LoadingView, hook_to_affect)
     else
-        mod:hook_enable(hook_to_affect)
+        mod:hook_enable(LoadingView, hook_to_affect)
     end
 end
